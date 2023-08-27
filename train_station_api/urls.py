0ae9ed759bf36f400cdc422adaa0c3102d0f5a9e
bythewaters <bythewaters800@gmail.com>
2023-08-27 20:40:01 +0200

    Added: urls for train and traintype using routers

--- train_station_api/urls.py
+++ train_station_api/urls.py
@@ -25,6 +25,7 @@ from drf_spectacular.views import (
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/train-station/user/", include("users.urls", namespace="users")),
+    path("api/train-station/trains/", include("trains.urls", namespace="trains")),
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path(
         "api/doc/swagger/",
---
+++ trains/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path, include
+from rest_framework import routers
+
+from trains.views import TrainTypeViewSet, TrainViewSet
+
+router = routers.DefaultRouter()
+router.register("train", TrainViewSet)
+router.register("train-type", TrainTypeViewSet)
+
+urlpatterns = [path("", include(router.urls))]
+
+app_name = "trains"
