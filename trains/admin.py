405f700f5a44f00f5fac52009f4803ee824efe25
bythewaters <bythewaters800@gmail.com>
2023-08-27 19:57:59 +0200

    Added: trains model to admin panel

--- trains/admin.py
+++ trains/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from django.contrib.admin import ModelAdmin
 
-# Register your models here.
+from trains.models import TrainType, Train
+
+
+@admin.register(TrainType)
+class TrainTypeAdmin(ModelAdmin):
+    list_display = (
+        "name",
+        "services"
+    )
+    ordering = ("name",)
+    list_filter = ("services",)
+
+
+@admin.register(Train)
+class TrainAdmin(ModelAdmin):
+    list_display = (
+        "name",
+        "cargo_place",
+        "places_in_cargo",
+        "train_type",
+    )
+    ordering = ("name",)
+    list_filter = ("train_type",)
