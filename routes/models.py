ad52a41c7cf8a3735f04552b26093f5f1562f59f
bythewaters <bythewaters800@gmail.com>
2023-09-02 23:22:11 +0200

    Added: models for Station and Route

--- routes/models.py
+++ routes/models.py
@@ -1,3 +1,20 @@
+from django.contrib.gis.db import models as gis_models
 from django.db import models
 
-# Create your models here.
+
+class Station(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    coordinate = gis_models.PointField(srid=4326)
+
+
+class Route(models.Model):
+    source = models.ForeignKey(
+        Station, related_name="route_source", on_delete=models.CASCADE
+    )
+    destination = models.ForeignKey(
+        Station, related_name="route_destination", on_delete=models.CASCADE
+    )
+    distance = models.IntegerField(null=True)
+
+    def __str__(self) -> str:
+        return f"{self.source} - {self.destination}"
