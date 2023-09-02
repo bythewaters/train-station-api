e348badcb6e8cabbc29d6be82d358a03aaf8b18f
bythewaters <bythewaters800@gmail.com>
2023-09-02 22:24:51 +0200

    Added: models for Journey and Crew

--- journies/models.py
+++ journies/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Crew(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+
+    def __str__(self) -> str:
+        return f"{self.first_name} {self.last_name}"
+
+
+class Journey(models.Model):
+    pass
