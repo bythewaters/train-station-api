932cae0b71e8bb31e16d1f52f63115fb2f1abead
bythewaters <bythewaters800@gmail.com>
2023-09-02 23:22:27 +0200

    Added: models to admin panel

--- routes/admin.py
+++ routes/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from django.contrib.admin import ModelAdmin
 
-# Register your models here.
+from routes.models import Station, Route
+
+
+@admin.register(Station)
+class TrainAdmin(ModelAdmin):
+    list_display = (
+        "name",
+    )
+    ordering = ("name",)
+
+
+@admin.register(Route)
+class TrainAdmin(ModelAdmin):
+    list_display = (
+        "source",
+        "destination",
+        "distance",
+    )
+    ordering = ("distance",)
