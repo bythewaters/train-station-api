d74bf85b018995b3153221d467691d85a7927857
bythewaters <bythewaters800@gmail.com>
2023-09-05 22:57:50 +0200

    Added: viewsets for crew and journey models

--- journies/views.py
+++ journies/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
 
-# Create your views here.
+from journies.models import Crew, Journey
+from journies.serializers import CrewSerializer, JourneySerializer
+
+
+class CrewView(viewsets.ModelViewSet):
+    queryset = Crew.objects.all()
+    serializer_class = CrewSerializer
+    permission_classes = (permissions.IsAdminUser,)
+
+
+class JourneyView(viewsets.ModelViewSet):
+    queryset = Journey.objects.all()
+    serializer_class = JourneySerializer
+    permission_classes = (permissions.IsAdminUser,)
