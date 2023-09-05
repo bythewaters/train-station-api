6c9773675d7d09678592a5d9e4adb3216ae31e44
bythewaters <bythewaters800@gmail.com>
2023-09-05 20:43:41 +0200

    Added: model journey and auto calculating arrival time

--- journies/models.py
+++ journies/models.py
@@ -1,4 +1,9 @@
+from datetime import timedelta, datetime
+
 from django.db import models
+
+from routes.models import Route
+from trains.models import Train
 
 
 class Crew(models.Model):
@@ -10,4 +15,32 @@ class Crew(models.Model):
 
 
 class Journey(models.Model):
-    pass
+    route = models.ForeignKey(
+        Route, on_delete=models.CASCADE, related_name="journey"
+    )
+    train = models.ForeignKey(
+        Train, on_delete=models.CASCADE, related_name="journey"
+    )
+    crew = models.ManyToManyField(Crew, related_name="journey")
+    departure_time = models.DateTimeField()
+    arrival_time = models.DateTimeField()
+
+    def calculate_arrival_time(self) -> datetime:
+        """
+        Auto calculate approximately arrival time train
+        :return:
+        arrival_time type datetime
+        """
+        duration = self.route.distance / self.train.train_type.max_speed
+        journey_duration = timedelta(hours=duration)
+        arrival_time = self.departure_time + journey_duration
+        return arrival_time
+
+    def save(self, *args, **kwargs):
+        if self.route and self.train and self.departure_time:
+            self.arrival_time = self.calculate_arrival_time()
+        super().save(*args, **kwargs)
+
+    def __str__(self) -> str:
+        return (f"{self.route.source.name} - "
+                f"{self.route.destination.name}")
