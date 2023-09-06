27f1b0c1bcb80b3d6baee48d2fa03c07c40e4adc
bythewaters <bythewaters800@gmail.com>
2023-09-06 08:36:56 +0200

    Added: trip_price field and calculate_price func

--- journies/models.py
+++ journies/models.py
@@ -8,6 +8,9 @@ from rest_framework.exceptions import ValidationError
 from routes.models import Route
 from trains.models import Train
 
+TRIP_PRICE_PER_ONE_KM = 0.10
+TAX = 0.2
+
 
 class Crew(models.Model):
     first_name = models.CharField(max_length=255)
@@ -28,6 +31,19 @@ class Journey(models.Model):
     crew = models.ManyToManyField(Crew, related_name="journey")
     departure_time = models.DateTimeField()
     arrival_time = models.DateTimeField(blank=True, null=True)
+    trip_price = models.FloatField()
+
+    def calculate_trip_price(self) -> float:
+        """
+        Auto calculate journey price
+        :return:
+        trip_price type float
+        """
+        price_services = len(self.train.train_type.services.all()) * 0.2
+        price_without_tax = (
+            self.route.distance * TRIP_PRICE_PER_ONE_KM + price_services
+        )
+        return price_without_tax + price_without_tax * TAX
 
     def calculate_arrival_time(self) -> datetime:
         """
@@ -45,17 +61,19 @@ class Journey(models.Model):
         arrival_time = self.departure_time + journey_duration
         return arrival_time
 
-    def save(self, *args, **kwargs):
+    def save(self, *args, **kwargs) -> None:
         self.clean()
         if self.route and self.train and self.departure_time:
             self.arrival_time = self.calculate_arrival_time()
+        if not self.trip_price:
+            self.trip_price = self.calculate_trip_price()
         super().save(*args, **kwargs)
 
     @staticmethod
     def validate_date(
         departure_time: datetime,
         error_to_raise: Type[ValidationError],
-    ):
+    ) -> None:
         if departure_time < timezone.now():
             raise error_to_raise("Departure time must be in the future.")
 
