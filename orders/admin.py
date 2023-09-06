d174da64394a7e80675e82ba8bdc4770a02a49f5
bythewaters <bythewaters800@gmail.com>
2023-09-06 09:06:54 +0200

    Added: order and ticket models and added to admin panel, also added validation to ticket model

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from orders.models import Order, Ticket
+
+admin.site.register(Order)
+admin.site.register(Ticket)
--- train_station_api/settings.py
+++ train_station_api/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     "trains",
     "routes",
     "journies",
+    "orders",
 ]
 
 MIDDLEWARE = [
